b7e9e491b9ebb87c522f00abc47fb03d69c7fb66
lycoris-mori <lukasgudonis7@gmail.com>
2026-05-13 17:13:42 +0300

    Pridėtas RPGEngine klasės antraštės failas
    Apibrėžta žaidimo logikos sąsaja, XP skalavimo ir lygio kėlimo metodai.

---
+++ RPGEngine.h
@@ -0,0 +1,27 @@
+#ifndef RPGENGINE_H_INCLUDED
+#define RPGENGINE_H_INCLUDED
+
+#include "Hero.cpp"
+#include "Task.h"
+
+class RPGEngine {
+    private:
+        int xpRequired;
+        void checkLevelUp(Hero &hero);
+
+    public:
+        RPGEngine() : xpRequired(100) {}
+
+        // Setters/getters
+        int getXPRequired() const;
+
+        // Method, used to process a successful task
+        void processTaskSuccess(Hero &hero, const Task &task);
+
+        // Method, used to process a failed task
+        void processTaskFailure(Hero &hero, const Task &task);
+};
+
+
+
+#endif // RPGENGINE_H_INCLUDED
